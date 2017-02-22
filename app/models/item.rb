0e5e5f8d928949b7cee19309f2997977054bbe9a
Cesar Pozo <c.pozomiranda@gmail.com>
2017-02-22 12:36:27 -0300

    Borrado

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,21 @@ class Item < ApplicationRecord
 	belongs_to :categorium, optional: true 
 	scope :size, ->(x){limit(x)}
 	scope :user, -> {where(usuario: false)}
+	after_destroy :borrar_todo
+
+
+	def borrar_todo
+		Categorium.all.each do |x|
+
+			unless x.items.any?
+				x.destroy
+			end
+		end
+	end
+
+
+
+
 end
+
+
--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -3,18 +3,24 @@ class Usuario < ApplicationRecord
 	validates :nombre, presence: true
 	validates :email, uniqueness: true
 	before_save :set_capitalize
+	after_destroy :borrar_usuario
 
 
 	def set_capitalize
 
 		Usuario.all.each do |cap|
-	cap.name = cap.name.capitalize
-	cap.save
-end
-
-		
+			cap.name = cap.name.capitalize
+			cap.save
+		end	
 	end
 
+	def borrar_usuario
+		Item.all.each do |x|
+
+			x.destroy
+			
+		end
+	end
 
 
 
